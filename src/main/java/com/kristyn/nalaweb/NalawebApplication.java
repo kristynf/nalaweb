1116dfb3e7cbd5a86d428ac87f885262c3b22cee
kristynf <kristynf@bu.edu>
2022-04-19 14:53:50 -0400

    initial commit

---
+++ src/main/java/com/kristyn/nalaweb/NalawebApplication.java
@@ -0,0 +1,13 @@
+package com.kristyn.nalaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NalawebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NalawebApplication.class, args);
+    }
+
+}
